d1dbc1e6fac8ebb7dd031f9cd077bcb7a8fd4896
Rakshit GL <rakshitgl@live.com>
2019-03-15 22:18:51 +0530

    UI Upgrade. Stabilized some functionalities.

--- static/editions.js
+++ static/editions.js
@@ -106,27 +106,41 @@ var i = 1;
 function setData(i){
   document.getElementById('question').innerText = questions[i].title;
   if(questions[i].markedForReview){
-    document.getElementById('question').style.color = 'violet';
+    document.getElementById("markedSymbol").style.visibility = 'visible';
   }
   else{
-    document.getElementById('question').style.color = 'red';
+    document.getElementById("markedSymbol").style.visibility = 'hidden';
   }
   questions[i].visited = true;
   document.getElementById('option1').value = questions[i].option1;
-  document.getElementById('option1').style.backgroundColor = 'blue';
+  document.getElementById('option1').style.backgroundColor = 'rgba(255,255,255,0.8)';
   document.getElementById('option2').value = questions[i].option2;
-  document.getElementById('option2').style.backgroundColor = 'blue';
+  document.getElementById('option2').style.backgroundColor = 'rgba(255,255,255,0.8)';
   document.getElementById('option3').value = questions[i].option3;
-  document.getElementById('option3').style.backgroundColor = 'blue';
+  document.getElementById('option3').style.backgroundColor = 'rgba(255,255,255,0.8)';
   document.getElementById('option4').value = questions[i].option4;
-  document.getElementById('option4').style.backgroundColor = 'blue';
-  document.getElementById('markRev').value = "Mark for Review";
+  document.getElementById('option4').style.backgroundColor = 'rgba(255,255,255,0.8)';
+  marking();
   if(!(questions[i].checked == false)){
-    document.getElementById(questions[i].checked).style.backgroundColor = 'green';
+    document.getElementById(questions[i].checked).style.backgroundColor = 'rgb(208, 121, 113)';
   }
   if(questions[i].markedForReview){
-    document.getElementById('markRev').value = "Unmark this Question";
+    unMarking();
   }
+}
+
+function marking(){
+  document.getElementById('markRev').innerText = "Mark for Review";
+  document.getElementById('marked').style.visibility = 'visible';
+  document.getElementById('unmarked').style.visibility = 'hidden';
+  document.getElementById('unmarked').style.paddingRight = '0px';
+}
+
+function unMarking(){
+  document.getElementById('markRev').innerText = "Unmark Question";
+  document.getElementById('marked').style.visibility = 'hidden';
+  document.getElementById('unmarked').style.visibility = 'visible';
+  document.getElementById('unmarked').style.paddingRight = '10px';
 }
 
 function nextQuestion(){
@@ -148,25 +162,40 @@ function prevQuestion(){
 function markQuestion(){
   if(!questions[i].markedForReview){
     questions[i].markedForReview = true;
-    document.getElementById('question').style.color = 'violet';
-    document.getElementById('markRev').value = "Unmark this Question";
+    document.getElementById("markedSymbol").style.visibility = 'visible';
+    unMarking();
   }
   else{
       questions[i].markedForReview = false;
-      document.getElementById('question').style.color = 'red';
-      document.getElementById('markRev').value = "Mark for Review";
+      document.getElementById("markedSymbol").style.visibility = 'hidden';
+      marking();
   }
 }
 
 function selectedOption(id){
-  if(!(questions[i].checked == false)){
-    document.getElementById(questions[i].checked).style.backgroundColor = 'blue';
+  if(!(questions[i].checked == false || questions[i].checked == id)){
+    document.getElementById(questions[i].checked).style.backgroundColor = 'rgba(255,255,255,0.8)';
   }
   if(id != questions[i].checked){
     questions[i].checked = id;
-    document.getElementById(questions[i].checked).style.backgroundColor = 'green';
+    document.getElementById(questions[i].checked).style.backgroundColor = 'rgb(137, 50, 42)';
   }
   else{
     questions[i].checked = false;
+    document.getElementById(id).style.backgroundColor = 'rgba(0,0,0,0.9)';
   }
+}
+
+function hovereffect(id){
+  if(questions[i].checked == false || questions[i].checked != id)
+    document.getElementById(id).style.backgroundColor = 'rgba(0,0,0,0.9)';
+  else
+    document.getElementById(id).style.backgroundColor = 'rgb(137, 50, 42)';
+}
+
+function dehover(id){
+  if(questions[i].checked == false || questions[i].checked != id)
+    document.getElementById(id).style.backgroundColor = 'rgba(255,255,255,0.8)';
+  else
+    document.getElementById(id).style.backgroundColor = 'rgb(208, 121, 113)';
 }
